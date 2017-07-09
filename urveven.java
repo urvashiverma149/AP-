19a692d6d47d59e1c903b86622ac89271adfc07e
urvashiverma149 <urvashiverma_kamlesh@srmuniv.edu.in>
2017-07-09 15:39:03 +0530

    Add files via upload

---
+++ urveven.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int m = s.nextInt();
+		if (m%10==0)
+	{ 
+		System.out.println(m+ "m is even");
+	}
+
+	else 
+	{
+		System.out.println(m+ "m is ood");
+	}
+}
+}
