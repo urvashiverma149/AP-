6c7f2081731a8f86308a3f0294375f9ee09508ef
urvashiverma149 <urvashiverma_kamlesh@srmuniv.edu.in>
2017-07-09 14:09:09 +0530

    Add files via upload

---
+++ uru.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int m = s.nextInt();
+		if (m>0)
+	{ 
+		System.out.println(m+ "m is positive");
+	}
+	else if(m<0)
+	{
+		System.out.println(m+ "m is negative");
+	}
+	else 
+	{
+		System.out.println(m+ "m is zero");
+	}
+}
+}
